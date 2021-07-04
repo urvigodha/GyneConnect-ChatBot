035ef599646ec15123197e39cec930e6469da751
urvigodha <urvi@iitg.ac.in>
2021-07-04 21:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,11 +22,11 @@ class Question(BaseModel):
 
 app = FastAPI()
 
-model = load_model('chatbot_model.h5')
-intents = json.loads(open('intents.json').read())
-words = pickle.load(open('words.pkl','rb'))
-classes = pickle.load(open('classes.pkl','rb'))
-new_Qs = pickle.load(open('new_questions','wb'))
+# model = load_model('chatbot_model.h5')
+# intents = json.loads(open('intents.json').read())
+# words = pickle.load(open('words.pkl','rb'))
+# classes = pickle.load(open('classes.pkl','rb'))
+# new_Qs = pickle.load(open('new_questions','wb'))
 # def clean_up_sentence(sentence):
 #     sentence_words = nltk.word_tokenize(sentence)
 #     sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
