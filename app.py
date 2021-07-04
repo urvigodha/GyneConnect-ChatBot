3580f8f53968b8d11a69da75a9ace273a32751f1
urvigodha <urvi@iitg.ac.in>
2021-07-04 20:38:28 +0530

    create API

---
+++ app.py
@@ -0,0 +1,86 @@
+import uvicorn
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Question(BaseModel):
+  question : str
+
+app = FastAPI()
+
+model = load_model('chatbot_model.h5')
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+new_Qs = pickle.load(open('new_questions','wb'))
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    if results[0][1] < 0.7 :
+      return [{"intent": "noanswer", "probability": str(0.7)}]
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, model)
+    res = getResponse(ints, intents)
+    if ints[0]['intent']=='noanswer':
+        new_Qs.append(msg)
+        pickle.dump(new_Qs,open('new_questions.pkl','wb'))
+    return res
+
+@app.get('/')
+def index():
+  return {'message': 'This is the homepage of the API '}
+
+@app.post('/get_answer')
+def get_answer(data:Question):
+  received = data.dict()
+  question = received['question']
+  ans = chatbot_response(question)
+  return{'answer':ans}
+
+@app.get('/unanswered_questions')
+def get_questions():
+  return {'unanswered_questions':new_Qs}
+
+if __name__ == '__main__':
+
+    uvicorn.run(app, host='127.0.0.1', port=4000, debug=True)
